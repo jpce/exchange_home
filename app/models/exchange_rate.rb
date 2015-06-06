a3181dfc2233098d2f809a9e70567c3c08e28e12
Jesus Lerma <shuyojl@gmail.com>
2015-06-06 13:02:51 -0500

    define custum relations

--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -1,2 +1,8 @@
 class ExchangeRate < ActiveRecord::Base
+	has_one :foreing_exchange_origin, 
+					foreing_key: 'foreing_exchange_orig_id',
+					class_name: 'ForeignExchange'
+	has_one :foreing_exchange_destination
+					foreing_key: 'foreing_exchange_dest_id',
+					class_name: 'ForeignExchange'
 end
--- app/models/foreign_exchange.rb
+++ app/models/foreign_exchange.rb
@@ -1,2 +1,6 @@
 class ForeignExchange < ActiveRecord::Base
+	belongs_to :foreing_exchange_origin, 
+					foreing_key: 'foreing_exchange_orig_id'
+	belongs_to :foreing_exchange_destination, 
+					foreing_key: 'foreing_exchange_dest_id'
 end
