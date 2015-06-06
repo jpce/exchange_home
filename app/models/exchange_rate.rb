b8dd54283f4f7ab6e8c12d177d48395ef088defd
Jesus Lerma <shuyojl@gmail.com>
2015-06-06 13:45:12 -0500

    solve problems with ExchangeRate relations

--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -1,9 +1,9 @@
 class ExchangeRate < ActiveRecord::Base
 	belongs_to :company
-	has_one :foreing_exchange_origin, 
-					foreing_key: 'foreing_exchange_orig_id',
+	belongs_to :foreing_exchange_origin, 
+					foreign_key: 'foreing_exchange_orig_id',
 					class_name: 'ForeignExchange'
-	has_one :foreing_exchange_destination
-					foreing_key: 'foreing_exchange_dest_id',
+	belongs_to :foreing_exchange_destination,
+					foreign_key: 'foreing_exchange_dest_id',
 					class_name: 'ForeignExchange'
 end
--- app/models/foreign_exchange.rb
+++ app/models/foreign_exchange.rb
@@ -1,8 +1,8 @@
 class ForeignExchange < ActiveRecord::Base
 	has_many :positions
 
-	belongs_to :foreing_exchange_origin, 
-					foreing_key: 'foreing_exchange_orig_id'
-	belongs_to :foreing_exchange_destination, 
-					foreing_key: 'foreing_exchange_dest_id'
+	has_many :foreing_exchange_origins, 
+					foreign_key: 'foreing_exchange_orig_id'
+	has_many :foreing_exchange_destinations, 
+					foreign_key: 'foreing_exchange_dest_id'
 end
